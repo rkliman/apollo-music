aa4e7884c9d6d76c2515324c6139d54992849a3e
Randall <rkliman11@gmail.com>
2026-08-01 23:41:06 -0700

    remove native tls from flake

--- flake.nix
+++ flake.nix
@@ -19,7 +19,7 @@
             lockFile = ./Cargo.lock;
           };
           nativeBuildInputs = [ pkgs.pkg-config ];
-          buildInputs = [ pkgs.sqlite pkgs.native-tls];
+          buildInputs = [ pkgs.sqlite ];
           meta = with pkgs.lib; {
             description = "Apollo music library CLI tool";
             license = licenses.mit;
